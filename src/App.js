24bd933ddcc2eef658e89ac010582c68ba40fca4
FashionEnglishName <995862101@qq.com>
2019-08-16 10:14:58 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import { addGun, removeGun, addGunAsync } from "./index.redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 组件不宜与外界有强耦合关系，所以此处不适合引入addGun, 应该在index.js中引入，再通过props传过来
+
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        return (
+            <div>
+                <h1>现在有机枪{this.props.num}把</h1>
+                <button onClick={this.props.addGun}>申请武器</button>
+                <button onClick={this.props.removeGun}>上交武器</button>
+                <button onClick={this.props.addGunAsync}>拖两天再给</button>
+            </div>
+        );
+    }
 }
 
+const mapStateToProps = (state) => {
+    return {num: state};
+};
+const mapActionCreatorToProps = { addGun, removeGun, addGunAsync };
+
+App = connect(mapStateToProps, mapActionCreatorToProps)(App);
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import { counter } from './index.redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(counter, composeEnhancers(
+    applyMiddleware(thunk)
+));
+
+ReactDom.render(
+    (<Provider store={store}>
+        <App />
+    </Provider>),
+    document.getElementById('root')
+);
+
+
+
+
